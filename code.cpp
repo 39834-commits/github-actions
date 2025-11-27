cfe57330d5fc32ae39850734540de5078debfcc7
39834-commits <39834@iqraisb.edu.pk>
2025-11-27 10:25:51 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,33 +1,6 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        cout << "Usage: ./program <num1> <operator> <num2>\n";
-        return 1;
-    }
-
-    double num1 = atof(argv[1]);
-    char op = argv[2][0];
-    double num2 = atof(argv[3]);
-    double result;
-
-    switch (op) {
-        case '+': result = num1 + num2; break;
-        case '-': result = num1 - num2; break;
-        case '*': result = num1 * num2; break;
-        case '/':
-            if (num2 == 0) {
-                cout << "Error: Division by zero!\n";
-                return 1;
-            }
-            result = num1 / num2; break;
-        default:
-            cout << "Invalid operator!\n";
-            return 1;
-    }
-
-    cout << "Result: " << result << endl;
-    return 0;
+int main(){
+    cout<<"hello world";
 }
