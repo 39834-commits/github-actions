cfdba34c2ebf5ec28bf4a1a099559cc2936ffc5d
39834-commits <39834@iqraisb.edu.pk>
2025-11-27 10:23:37 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,40 +1,30 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-int main() {
-    double num1, num2;
-    char op;
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        cout << "Usage: ./program <num1> <operator> <num2>\n";
+        return 1;
+    }
 
-    cout << "Enter first number: ";
-    cin >> num1;
-
-    cout << "Enter operator (+, -, *, /): ";
-    cin >> op;
-
-    cout << "Enter second number: ";
-    cin >> num2;
-
+    double num1 = atof(argv[1]);
+    char op = argv[2][0];
+    double num2 = atof(argv[3]);
     double result;
 
     switch (op) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
         case '/':
             if (num2 == 0) {
-                cout << "Error: Division by zero!" << endl;
+                cout << "Error: Division by zero!\n";
                 return 1;
             }
-            result = num1 / num2;
-            break;
+            result = num1 / num2; break;
         default:
-            cout << "Invalid operator!" << endl;
+            cout << "Invalid operator!\n";
             return 1;
     }
 
